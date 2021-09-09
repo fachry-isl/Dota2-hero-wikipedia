bb934a1a6a891889972f562d5d445d57a4ae0f79
FachryIkhsal890 <fachryikhsal890@gmail.com>
2021-09-09 20:06:47 +0700

    AboutActivity: Add function to share to Whatsapp activity_detail.xml: Add fillViewPort for scroll view so screen match with parent

--- app/src/main/java/com/boredom/project_5_submission/DetailActivity.kt
+++ app/src/main/java/com/boredom/project_5_submission/DetailActivity.kt
@@ -1,16 +1,22 @@
 package com.boredom.project_5_submission
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 
-class DetailActivity: AppCompatActivity() {
+class DetailActivity : AppCompatActivity() {
     var gambarHeroDetail: ImageView? = null
     var namaHeroDetail: TextView? = null
     var contentDetailHero: TextView? = null
+
+    lateinit var btn_share: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +29,7 @@ class DetailActivity: AppCompatActivity() {
 
         // Mengambil data dari explicit intent untuk ditampil pada halaman ini
         incomingExtra
+
     }
 
     override fun onSupportNavigateUp(): Boolean {
@@ -31,14 +38,34 @@ class DetailActivity: AppCompatActivity() {
     }
 
     private val incomingExtra: Unit
-        private get() {
+     get() {
             if (intent.hasExtra("img_hero") && intent.hasExtra("nama_hero") && intent.hasExtra("info_hero")) {
                 val fotoHero = intent.getIntExtra("img_hero", 0)
                 val namaHero = intent.getStringExtra("nama_hero")
                 val infoHero = intent.getStringExtra("info_hero")
                 setDataActivity(fotoHero, namaHero, infoHero)
+
+                btn_share = findViewById(R.id.btn_share)
+                btn_share.setOnClickListener{
+                    val shareIntent = Intent()
+                    shareIntent.setAction(Intent.ACTION_SEND)
+                    shareIntent.setType("text/plain")
+                    //Target Whatsapp
+                    shareIntent.setPackage("com.whatsapp")
+                    //Add Text
+                    val formatText= "Lore $namaHero from Dota Wikipedia : $infoHero"
+                    shareIntent.putExtra(Intent.EXTRA_TEXT, formatText)
+                    shareIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+                    try {
+                        startActivity(shareIntent)
+                    } catch (ex: ActivityNotFoundException) {
+                        Toast.makeText(this,"Whatsapp have not been installed",Toast.LENGTH_LONG).show()
+                        //ToastHelper.MakeShortText("Whatsapp have not been installed.")
+                    }
+                }
             }
         }
+
 
     private fun setDataActivity(fotoHero: Int, namaHero: String?, infoHero: String?) {
         Glide.with(this).load(fotoHero).apply(
@@ -49,4 +76,5 @@ class DetailActivity: AppCompatActivity() {
         namaHeroDetail!!.text = namaHero
         contentDetailHero!!.text = infoHero
     }
+
 }
